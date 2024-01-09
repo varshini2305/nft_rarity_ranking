9fc37a29f2950b68daace32934e301cd58a0ac00
varshini_balaji <varshinibalaji2305@gmail.com>
2024-01-09 20:40:27 +0530

    update core

--- run_batches.py
+++ run_batches.py
@@ -7,20 +7,31 @@ from tqdm import tqdm
 from functools import lru_cache
 
 # Read the dataframe from pickle file
-url_df = pd.read_pickle("data/url_df.pkl")
-urls = url_df.values.tolist()
+# url_df = pd.read_pickle("data/url_df.pkl")
+# urls = url_df.values.tolist()
+
+remaining_urls_df = pd.read_pickle("data/remaining_urls_df.pkl")
+urls = remaining_urls_df['url'].values.tolist()
+
+
+
 
 # Command line argument parser
 parser = argparse.ArgumentParser(description="Parallel script for fetching data.")
 parser.add_argument("--start", type=int, help="Start index or position in the list", default=0)
-parser.add_argument("--stop", type=int, help="Stop index or position in the list", default=len(urls))
+parser.add_argument("--size", type=int, help="No of urls ot fetch for", default=len(urls))
 parser.add_argument("--batch_size", type=int, help="Batch size for writing output files", default=100)
-parser.add_argument("--output_path", type=str, help="Custom output path for data files", default="data/")
+# parser.add_argument("--output_path", type=str, help="Custom output path for data files", default="data/")
+
+
 
 args = parser.parse_args()
 
+
 # Extract relevant URLs based on start and stop indices
-selected_urls = urls[args.start:args.stop]
+selected_urls = urls[args.start:args.start+args.size]
+
+print(f"{len(urls)=}, {len(selected_urls)=}, {args.size=}, {args.start=}, args.end={args.start+args.size}")
 
 # Initialize global variables
 count = 0
@@ -30,7 +41,9 @@ headers = {
     'accept': 'application/json',
     'origin': 'https://raritysniper.com'
 }
+
 nfts_list = []
+nfts_df = pd.DataFrame()
 
 @lru_cache(maxsize=None)
 async def fetch_data(session, url, collection_slug, i, progress_bar):
@@ -46,18 +59,20 @@ async def fetch_data(session, url, collection_slug, i, progress_bar):
     count += 1
 
     progress_bar.update(1)
-
+    
     if count % args.batch_size == 0:
+        
+        if nfts_df.empty is False:
+            nfts_df.to_pickle(f"data/prev_nfts_df_{args.start}_{args.start+args.size}.pkl")
+
         nfts_df = pd.DataFrame(nfts_list)
-        batch_num = count // args.batch_size
-        output_file_path = f"{args.output_path}_{batch_num}.pkl"
-        nfts_df.to_pickle(output_file_path)
+        nfts_df.to_pickle(f"data/nfts_df_{args.start}_{args.start+args.size}.pkl")
 
 async def bound_fetch(sem, func):
     async with sem:
         return await func
 
-max_concurrent_requests = 32
+max_concurrent_requests = 15
 
 async def main():
     async with aiohttp.ClientSession() as session:
@@ -70,11 +85,11 @@ async def main():
             # Save the final batch if not a multiple of batch size
             if nfts_list:
                 nfts_df = pd.DataFrame(nfts_list)
-                output_file_path = f"{args.output_path}_combined_batch.pkl"
-                nfts_df.to_pickle(output_file_path)
+                nfts_df.to_pickle(f"data/aggregated_nfts_df_{args.start}_{args.start+args.size}.pkl")
 
 if __name__ == "__main__":
     asyncio.run(main())
 
 
-# python run_batches.py --start 400000 --stop 500000 --batch_size 1000 --output_path data/4l_nft_df
+# python run_batches.py --start 0 --size 1500000 --batch_size 1000
+# python run_batches.py --start 1500000 --size 1500000 --batch_size 1000
